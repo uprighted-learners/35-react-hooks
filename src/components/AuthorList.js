306c9f432a3332e6ad1ce44e120535220bf7af16
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-25 19:29:43 -0500

    some error handling

--- src/components/AuthorList.js
+++ src/components/AuthorList.js
@@ -9,10 +9,19 @@ export default function AuthorList() {
 
     const fetchData = () => {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
             .then(res => {
-                console.log(res)
-                setAuthors(res)
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error("SOMETHING WENT WRONG!")
+                }
+            })
+            .then(res => {
+                if (res.length > 0) {
+                    setAuthors(res)
+                } else {
+                    alert("NO DATA!")
+                }
             })
             .catch((error) => {
                 alert("SOMETHING IS WRONG!")
